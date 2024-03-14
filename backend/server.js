0080b81b041e7fee77e544e60832bc8a8aa0aa66
S EVLYN MONICA JOSEPH <126934852+EVLYNMONICAJOSEPH@users.noreply.github.com>
2024-03-14 12:00:08 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const routes = require("./routes");
 main().catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect("mongodb://admin:admin@40.121.164.186:27017/todos",
+  await mongoose.connect("mongodb_evlyn://admin:admin@40.121.164.186:27017/todos",
     useUnifiedTopology: true,
     useNewUrlParser: true,
   });
